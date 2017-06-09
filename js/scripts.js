fd13aa9e8c801bc87e91e42addc4cd6502721748
Emilie Thoreson <thoresonemilie@gmail.com>
2017-06-09 09:11:25 -0700

    refactor, seperate business and user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 // business logic:
-
+var pingPongLogic = function(number) {
+  for (var index = 1; index <= number; index += 1) {
+    if (index % 15 === 0) {
+      $("ul#output").append("<li>pingpong</li>");
+    } else if (index % 5 === 0) {
+      $("ul#output").append("<li>pong</li>");
+    } else if (index % 3 === 0) {
+      $("ul#output").append("<li>ping</li>");
+    } else {
+      $("ul#output").append("<li>" + index + "</li>");
+    }
+  }
+}
 
 
 
@@ -11,20 +23,6 @@ $(document).ready(function() {
   event.preventDefault();
   var stringInput = $("#number").val();
   var integer = parseInt(stringInput);
-  var pingPongLogic = function(number) {
-    for (var index = 1; index <= number; index += 1) {
-      if (index % 15 === 0) {
-        $("ul#output").append("<li>pingpong</li>");
-      } else if (index % 5 === 0) {
-        $("ul#output").append("<li>pong</li>");
-      } else if (index % 3 === 0) {
-        $("ul#output").append("<li>ping</li>");
-      } else {
-        $("ul#output").append("<li>" + index + "</li>");
-      }
-    }
-  }
-
   console.log(pingPongLogic(integer));
   });
 });
