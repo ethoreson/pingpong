a6addcf14ed019aaea307d621a89b5b5f68dc3f2
Emilie Thoreson <thoresonemilie@gmail.com>
2017-06-09 09:07:27 -0700

    fix integer/number bug

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,15 @@ $(document).ready(function() {
   var integer = parseInt(stringInput);
   var pingPongLogic = function(number) {
     for (var index = 1; index <= number; index += 1) {
-        if (number % 15 === 0) {
-          $("ul#output").append("<li>pingpong</li>");
-          console.log("pingpong")
-        } else if (number % 5 === 0) {
-          $("ul#output").append("<li>pong</li>");
-          console.log("pong")
-        } else if (number % 3 === 0) {
-          $("ul#output").append("<li>ping</li>");
-          console.log("ping")
-        } else {
-          $("ul#output").append("<li>" + number + "</li>");
-          console.log(number);
-        }
+      if (index % 15 === 0) {
+        $("ul#output").append("<li>pingpong</li>");
+      } else if (index % 5 === 0) {
+        $("ul#output").append("<li>pong</li>");
+      } else if (index % 3 === 0) {
+        $("ul#output").append("<li>ping</li>");
+      } else {
+        $("ul#output").append("<li>" + index + "</li>");
+      }
     }
   }
 
